c7621672eb0627fa870149212754c69b6bfde484
SamuelRab <samuelrabelodasilva@gmail.com>
2025-12-01 12:54:43 -0300

    Finalizado

--- src/models/entregaModel.js
+++ src/models/entregaModel.js
@@ -10,14 +10,14 @@ const entregaModel = {
      * Insere a entrega com os valores calculados.
      * Faz Commit se tudo der certo ou Rollback se der erro.
      * * @async
-     * @param {Object} dados - Dados brutos recebidos da requisição.
-     * @param {Object} calculos - Objeto com os valores finais já calculados pelo controller.
-     * @param {number} calculos.distancia - Valor monetário da distância.
-     * @param {number} calculos.peso - Valor monetário do peso.
-     * @param {number} calculos.acrescimo - Valor do acréscimo.
-     * @param {number} calculos.desconto - Valor do desconto.
-     * @param {number} calculos.taxa - Valor da taxa extra.
-     * @param {number} calculos.final - Valor total final a ser cobrado.
+     * @param {Object} dados Dados brutos recebidos da requisição.
+     * @param {Object} calculos Objeto com os valores finais já calculados pelo controller.
+     * @param {number} calculos.distancia Valor monetário da distância.
+     * @param {number} calculos.peso Valor monetário do peso.
+     * @param {number} calculos.acrescimo Valor do acréscimo.
+     * @param {number} calculos.desconto Valor do desconto.
+     * @param {number} calculos.taxa Valor da taxa extra.
+     * @param {number} calculos.final Valor total final a ser cobrado.
      * @returns {Promise<object>} Retorna o ID do pedido criado e o valor final.
      * @throws {Error} Lança erro caso a transação falhe, forçando o tratamento no controller.
      */
