41b98a59585555d16c593b0525112021fb525a3f
SamuelRab <samuelrabelodasilva@gmail.com>
2025-12-08 07:41:31 -0300

    Finalização

--- src/models/entregaModel.js
+++ src/models/entregaModel.js
@@ -11,13 +11,13 @@ const entregaModel = {
      * Faz Commit se tudo der certo ou Rollback se der erro.
      * * @async
      * @param {Object} dados Dados brutos recebidos da requisição.
-     * @param {Object} calculos Objeto com os valores finais já calculados pelo controller.
-     * @param {number} calculos.distancia Valor monetário da distância.
-     * @param {number} calculos.peso Valor monetário do peso.
-     * @param {number} calculos.acrescimo Valor do acréscimo.
-     * @param {number} calculos.desconto Valor do desconto.
-     * @param {number} calculos.taxa Valor da taxa extra.
-     * @param {number} calculos.final Valor total final a ser cobrado.
+     * @param {Object} Objeto com os valores finais já calculados pelo controller.
+     * @param {number} distancia Valor monetário da distância.
+     * @param {number} peso Valor monetário do peso.
+     * @param {number} acrescimo Valor do acréscimo.
+     * @param {number} desconto Valor do desconto.
+     * @param {number} taxa Valor da taxa extra.
+     * @param {number} final Valor total final a ser cobrado.
      * @returns {Promise<object>} Retorna o ID do pedido criado e o valor final.
      * @throws {Error} Lança erro caso a transação falhe, forçando o tratamento no controller.
      */
@@ -43,9 +43,9 @@ const entregaModel = {
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
             
             const valuesEntrega = [
-                idPedido, calculos.distancia, calculos.peso, 
-                calculos.acrescimo, calculos.desconto, calculos.taxa, 
-                calculos.final, 'calculado'
+                idPedido, distancia, peso, 
+                acrescimo, desconto, taxa, 
+                final, 'calculado'
             ];
 
             await connection.query(sqlEntrega, valuesEntrega);
